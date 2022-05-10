4405c8f23499b30768e838a380a583ae24187a95
kimi901116 <79627914+kimi901116@users.noreply.github.com>
2022-05-10 11:54:26 +0800

    Add files via upload

---
+++ week12_double_TRT/main.cpp
@@ -0,0 +1,56 @@
+#include <GL/glut.h>
+float angle = 0;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glColor3f(1,1,1);
+    glutSolidTeapot(0.3);
+
+    glPushMatrix();
+
+    glTranslatef(0.2,0,0);
+    glRotatef(angle,0,0,1);
+    glTranslatef(0.2,0,0);
+    glColor3f(1,0,0);
+    glutSolidTeapot(0.2);
+
+        glPushMatrix();
+        glTranslatef(0.2,0,0);
+        glRotatef(angle,0,0,1);
+        glTranslatef(0.2,0,0);
+        glColor3f(1,0,0);
+        glutSolidTeapot(0.2);
+        glPopMatrix();
+
+    glPopMatrix();
+
+    glPushMatrix();
+
+    glTranslatef(-0.2,0,0);
+    glRotatef(angle,0,0,1);
+    glTranslatef(-0.2,0,0);
+    glColor3f(1,0,0);
+    glutSolidTeapot(0.2);
+
+        glPushMatrix();
+        glTranslatef(-0.2,0,0);
+        glRotatef(-angle,0,0,1);
+        glTranslatef(-0.2,0,0);
+        glColor3f(1,0,0);
+        glutSolidTeapot(0.2);
+        glPopMatrix();
+
+    glPopMatrix();
+
+    glutSwapBuffers();
+    angle+=0.01;
+}
+int main(int argc,char**argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week12_TRT");
+    glutIdleFunc(display);
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
